a31c75f25e60faf1b7d4f9ce17f3d13ad35a9eb8
Wellington Leite <wellingtonleitee.s@gmail.com>
2022-12-21 07:19:58 -0300

    some tasks resolved

--- README.md
+++ README.md
@@ -8,16 +8,16 @@
 
 * Demo video - You can view how the completed functionality should look at: https://drive.google.com/file/d/1o2Rz5HBOPOEp9DlvE9FWnLJoW9KUp5-C/view?usp=sharing
 
-- [ ] Once the <Checkout /> component is mounted, load the products using the getProducts function.
-- [ ] Once all the data is successfully loaded, hide the loading icon.
-- [ ] Render each product object as a <Product/> component, passing in the necessary props.
-- [ ] Implement the following functionality:
-- [ ] The add and remove buttons should adjust the ordered quantity of each product
-- [ ] The add and remove buttons should be enabled/disabled to ensure that the ordered quantity can’t be negative and can’t exceed the available count for that product.
-- [ ] The total shown for each product should be calculated based on the ordered quantity and the price
-- [ ] The total in the order summary should be calculated
-- [ ] For orders over $1000, apply a 10% discount to the order. Display the discount text only if a discount has been applied.
-- [ ] The total should reflect any discount that has been applied
+- [x] Once the <Checkout /> component is mounted, load the products using the getProducts function.
+- [x] Once all the data is successfully loaded, hide the loading icon.
+- [x] Render each product object as a <Product/> component, passing in the necessary props.
+- [x] Implement the following functionality:
+- [x] The add and remove buttons should adjust the ordered quantity of each product
+- [x] The add and remove buttons should be enabled/disabled to ensure that the ordered quantity can’t be negative and can’t exceed the available count for that product.
+- [x] The total shown for each product should be calculated based on the ordered quantity and the price
+- [x] The total in the order summary should be calculated
+- [x] For orders over $1000, apply a 10% discount to the order. Display the discount text only if a discount has been applied.
+- [x] The total should reflect any discount that has been applied
 - [ ] All dollar amounts should be displayed to 2 decimal places
 
 ---
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,18 @@
-import styles from './Checkout.module.css';
-import { LoadingIcon } from './Icons';
-import { getProducts } from './dataService';
+import styles from "./Checkout.module.css";
+import { LoadingIcon } from "./Icons";
+import { getProducts } from "./dataService";
+import { useEffect, useState } from "react";
 
-const Product = ({ id, name, availableCount, price, orderedQuantity, total }) => {
+const Product = ({
+  id,
+  name,
+  availableCount,
+  price,
+  orderedQuantity,
+  total,
+  addProduct,
+  removeProduct,
+}) => {
   return (
     <tr>
       <td>{id}</td>
@@ -12,22 +22,100 @@ const Product = ({ id, name, availableCount, price, orderedQuantity, total }) =>
       <td>{orderedQuantity}</td>
       <td>${total}</td>
       <td>
-        <button className={styles.actionButton}>+</button>
-        <button className={styles.actionButton}>-</button>
+        <button
+          className={styles.actionButton}
+          disabled={orderedQuantity === availableCount}
+          onClick={() => addProduct(id)}
+        >
+          +
+        </button>
+        <button
+          className={styles.actionButton}
+          disabled={!orderedQuantity}
+          onClick={() => removeProduct(id)}
+        >
+          -
+        </button>
       </td>
     </tr>
   );
-}
-
+};
 
 const Checkout = () => {
+  const [data, setData] = useState({
+    products: [],
+    loading: true,
+  });
+
+  const handleProducts = async () => {
+    const response = await getProducts();
+
+    const products = response.map(product => ({
+      ...product,
+      orderedQuantity: 0,
+      total: 0,
+    }));
+
+    setData({ products, loading: false });
+  };
+
+  const applyDiscount = (product) => {
+    if (product.total > 1000) {
+      const totalWithDiscount = product.total - (product.total * 10) / 100;
+
+      Object.assign(product, {
+        total: totalWithDiscount,
+      });
+    }
+  }
+
+  const addProduct = (id) => {
+    const index = data.products.findIndex((item) => item.id === id);
+
+    let newProducts = data.products;
+
+    const product = newProducts[index];
+
+    Object.assign(product, {
+      orderedQuantity: product.orderedQuantity + 1,
+      total: product.price * (product.orderedQuantity + 1),
+    });
+
+    applyDiscount(product);
+
+    setData({ loading: false, products: newProducts });
+  };
+
+  const removeProduct = (id) => {
+    const index = data.products.findIndex((item) => item.id === id);
+
+    let newProducts = data.products;
+
+    const product = newProducts[index];
+
+    Object.assign(product, {
+      orderedQuantity: product.orderedQuantity - 1,
+      total: product.price * (product.orderedQuantity - 1),
+    });
+
+    applyDiscount(product);
+
+    setData({ loading: false, products: newProducts });
+  };
+
+  const total = data.products.reduce((acumulate, product) => acumulate + product.total, 0);
+
+  useEffect(() => {
+    handleProducts();
+  }, []);
+
   return (
     <div>
       <header className={styles.header}>
         <h1>Electro World</h1>
       </header>
       <main>
-        <LoadingIcon />
+        {data.loading && <LoadingIcon />}
         <table className={styles.table}>
           <thead>
             <tr>
@@ -42,12 +130,19 @@ const Checkout = () => {
             </tr>
           </thead>
           <tbody>
-          {/* Products should be rendered here */}
+            {data.products.map((product) => (
+              <Product
+                key={product.id}
+                addProduct={addProduct}
+                removeProduct={removeProduct}
+                {...product}
+              />
+            ))}
           </tbody>
         </table>
         <h2>Order summary</h2>
         <p>Discount: $ </p>
-        <p>Total: $ </p>
+        <p>Total: ${total} </p>
       </main>
     </div>
   );
